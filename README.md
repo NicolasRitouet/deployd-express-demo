9388baec9f52bd5cb621392332b55c8b86ef1c2b
NicolasRitouet <nicolas@ritouet.com>
2015-01-28 11:46:58 +0100

    This is where it all begins... :baby:

---
+++ README.md
@@ -0,0 +1,20 @@
+# Express-Deployd demo
+<<<<<<< HEAD
+=======
+
+> Demo of Deployd used as an Express middleware
+
+
+## Getting started
+
+### Requirements
+
+* [nodejs / npm](http://nodejs.org/)
+* A running [Mongodb](http://www.mongodb.org/downloads)
+
+````
+npm install
+npm start
+````
+Browse to [http://localhost:3000/dashboard](http://localhost:3000/dashboard) to see the dashboard.
+>>>>>>> 8c72f2d... Add dashboard link in README.md
---
+++ index.js
@@ -0,0 +1,28 @@
+"use strict";
+
+var PORT = process.env.PORT || 3000;
+var ENV = process.env.NODE_ENV || 'development';
+
+// setup express
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+});
+
+// setup deployd
+require('deployd').attach(server, {
+  env: ENV,
+  db: {host:'localhost', port:27017, name:'test-app'}
+});
+
+// After attach, express can use server.handleRequest as middleware
+app.use(server.handleRequest);
+
+// start server
+server.listen(PORT, function() {
+  console.log('Express & Deployd listening at http://%s:%s', server.address().address, server.address().port)
+});
