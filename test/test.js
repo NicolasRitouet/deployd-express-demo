3becfa65f6f3ba0f061b44df0d1a9763306306b5
NicolasRitouet <nicolas@ritouet.com>
2015-01-29 16:24:40 +0100

    :white_check_mark: Add test for public folder

--- test/test.js
+++ test/test.js
@@ -1,6 +1,6 @@
 var request = require('supertest'),
 expect = require('chai').expect;
-var express = require('../index').app;
+var express = require('../server').app;
 
 describe('GET /contacts', function() {
   it('respond with json', function(done) {
@@ -22,6 +22,21 @@ describe('display dashboard', function(){
     .end(function(err, res){
       if (err) return done(err);
       expect(res.text).to.contain('<title>Deployd Dashboard</title>');
+      done()
+    });
+  });
+});
+
+
+describe('display public folder', function(){
+  it('contains correct title', function(done) {
+
+    request(express)
+    .get('/')
+    .expect(200)
+    .end(function(err, res){
+      if (err) return done(err);
+      expect(res.text).to.contain('<title>deployd-express-demo</title>');
       done()
     });
   });
