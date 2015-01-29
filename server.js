450e5dcdd7086b03466bde9896ecfc8bba905746
NicolasRitouet <nicolas@ritouet.com>
2015-01-29 16:50:12 +0100

    :green_heart: Deploy on Heroku

--- server.js
+++ server.js
@@ -1,25 +1,48 @@
 "use strict";
 
+var express = require('express');
+var http = require('http');
+
 var PORT = process.env.PORT || 3000;
 var ENV = process.env.NODE_ENV || 'development';
 
 var MONGODB_HOST = process.env.WERCKER_MONGODB_HOST || 'localhost';
 var MONGODB_PORT = process.env.WERCKER_MONGODB_PORT || 27017;
+var MONGODB_DB = 'test-app';
+var MONGODB_USERNAME = '';
+var MONGODB_PASS = '';
+
+// On Heroku, Mongolab add this variable to
+if (process.env.MONGOLAB_URI) {
+  var uriObject = require('mongodb-uri').parse(process.env.MONGOLAB_URI);
+  MONGODB_HOST = uriObject.hosts[0].host;
+  MONGODB_PORT = uriObject.hosts[0].port;
+  MONGODB_DB = uriObject.database;
+  MONGODB_USERNAME = uriObject.username;
+  MONGODB_PASS = uriObject.password;
+}
 
 // setup express
-var express = require('express');
 var app = exports.app = express();
-var server = require('http').createServer(app);
+var server = http.createServer(app);
 
-
+// Define a new route
 app.get('/hello-express', function (req, res) {
-  res.send('Hello World!')
+  res.send('Hello World!');
 });
 
 // setup deployd
 require('deployd').attach(server, {
   env: ENV,
-  db: {host: MONGODB_HOST, port: MONGODB_PORT, name: 'test-app'}
+  db: {
+    host: MONGODB_HOST,
+    port: MONGODB_PORT,
+    name: MONGODB_DB,
+    credentials: {
+      username: MONGODB_USERNAME,
+      password: MONGODB_PASS
+    }
+  }
 });
 
 // After attach, express can use server.handleRequest as middleware
@@ -28,5 +51,5 @@ app.use(server.handleRequest);
 // start server
 server.listen(PORT, function() {
   var serverAddr = server.address().address == '0.0.0.0' ? 'localhost' : server.address().address;
-  console.log('Express & Deployd started.\n\nPlease visit http://%s:%s', serverAddr, server.address().port)
+  console.log('Express & Deployd started.\n\nPlease visit http://%s:%s', serverAddr, server.address().port);
 });
